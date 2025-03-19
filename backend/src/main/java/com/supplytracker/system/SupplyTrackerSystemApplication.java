48fc183e87efae19302301accf82c3cc9594cc5b
Thiago Trotta <trottathiago1@gmail.com>
2025-03-19 17:36:15 -0300

    build: initialize Spring Boot project with dependencies

---
+++ backend/src/main/java/com/supplytracker/system/SupplyTrackerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.supplytracker.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyTrackerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyTrackerSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/supplytracker/system/SupplyTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplytracker.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyTrackerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
